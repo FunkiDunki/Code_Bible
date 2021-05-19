fcb9c20b9c6b3c716b7b48e4f405a603018ab609
FunkiDunki <72818204+FunkiDunki@users.noreply.github.com>
2021-05-18 20:38:24 -0700

    Create BoardGrabber.py

---
+++ Sudoku/BoardGrabber.py
@@ -0,0 +1,33 @@
+
+import mechanicalsoup
+from Sudoku import *
+
+def grabSudoku(id=9706885399):
+	url = f"https://nine.websudoku.com/?level=4&set_id={id}"
+	browser = mechanicalsoup.Browser()
+
+	page=browser.get(url)
+
+	board = [[0 for i in range(9)] for j in range(9)]
+
+	for row in range(9):
+		for col in range(9):
+			cell = page.soup.find("input",id=f"f{col}{row}")
+			if(cell["class"]==["d0"]):
+				board[row][col] = 0
+			else:
+				board[row][col] = int(cell["value"][0])
+	print(board)
+	return board
+
+if __name__ == '__main__':
+	id = int(input("What is the id number of your puzzle?: "))
+	board=grabSudoku(id)
+	s = Solver(board)
+	print("Here is the unsolved puzzle:")
+	s.display()
+	s.go()
+	print("\nHere is the solved version of the puzzle:")
+	s.display()
+	print("\nHere is the listed version to put into the autohotkey script: ")
+	s.display(mode="listed")
